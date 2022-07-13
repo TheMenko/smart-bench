14aab295bbdf72983b7ffcdeae9972e243583d92
TheMenko <themenkoprojects@gmail.com>
2022-07-13 15:15:28 +0200

    add stuff

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -24,7 +24,6 @@ pub trait InkMessage: codec::Encode {
     const SELECTOR: [u8; 4];
 }
 
-smart_bench_macro::contract!("./contracts/computation.contract");
 smart_bench_macro::contract!("./contracts/pendulum_amm.contract");
 
 pub async fn exec(cli: Cli) -> color_eyre::Result<()> {
@@ -38,37 +37,12 @@ pub async fn exec(cli: Cli) -> color_eyre::Result<()> {
 
     for contract in &cli.contracts {
         match contract {
-            Contract::OddProduct => {
-                let computation_new = computation::constructors::new();
-                let computation_odd_product = || computation::messages::odd_product(1000).into();
-                runner
-                    .prepare_contract(
-                        "computation",
-                        computation_new,
-                        cli.instance_count,
-                        computation_odd_product,
-                    )
-                    .await?;
-            }
-            Contract::TriangleNumber => {
-                let computation_new = computation::constructors::new();
-                let computation_triangle_number =
-                    || computation::messages::triangle_number(1000).into();
-                runner
-                    .prepare_contract(
-                        "computation",
-                        computation_new,
-                        cli.instance_count,
-                        computation_triangle_number,
-                    )
-                    .await?;
-            }
             Contract::SetFeeTo => {
                 let pen_constructor = pendulum_amm::constructors::new(
                     "USDC".to_string(),
-                    "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY".to_string(),
+                    "GAKNDFRRWA3RPWNLTI3G4EBSD3RGNZZOY5WKWYMQ6CQTG3KIEKPYWAYC".to_string(),
                     "EUR ".to_string(),
-                    "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty".to_string(),
+                    "GAKNDFRRWA3RPWNLTI3G4EBSD3RGNZZOY5WKWYMQ6CQTG3KIEKPYWAYC".to_string(),
                 );
                 let pen_set_fee_to = || pendulum_amm::messages::set_fee_to(bob.clone()).into();
 
@@ -78,6 +52,23 @@ pub async fn exec(cli: Cli) -> color_eyre::Result<()> {
                         pen_constructor,
                         cli.instance_count,
                         pen_set_fee_to,
+                    )
+                    .await?;
+            }
+            Contract::GetReserves => {
+                let pen_constructor = pendulum_amm::constructors::new(
+                    "USDC".to_string(),
+                    "GAKNDFRRWA3RPWNLTI3G4EBSD3RGNZZOY5WKWYMQ6CQTG3KIEKPYWAYC".to_string(),
+                    "EUR ".to_string(),
+                    "GAKNDFRRWA3RPWNLTI3G4EBSD3RGNZZOY5WKWYMQ6CQTG3KIEKPYWAYC".to_string(),
+                );
+                let pen_get_reserves = || pendulum_amm::messages::get_reserves().into();
+                runner
+                    .prepare_contract(
+                        "pendulum_amm",
+                        pen_constructor,
+                        cli.instance_count,
+                        pen_get_reserves,
                     )
                     .await?;
             }
